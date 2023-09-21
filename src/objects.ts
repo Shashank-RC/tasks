99e6e31bb46868f6cf376550b87dd8ae38816661
Shashank-RC <shashankreddy0402@gmail.com>
2023-09-21 12:15:16 -0400

    object.ts functions

--- src/objects.ts
+++ src/objects.ts
@@ -10,9 +10,17 @@ export function makeBlankQuestion(
     name: string,
     type: QuestionType
 ): Question {
-    return {};
+    return {
+        id,
+        name,
+        type,
+        body: "",
+        expected: "",
+        options: [],
+        points: 1,
+        published: false
+    };
 }
-
 /**
  * Consumes a question and a potential `answer`, and returns whether or not
  * the `answer` is correct. You should check that the `answer` is equal to
@@ -21,7 +29,9 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    const correct = question.expected.toLowerCase().trim();
+    const finalAnswer = answer.toLowerCase().trim();
+    return finalAnswer === correct;
 }
 
 /**
@@ -31,7 +41,16 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type === "short_answer_question") {
+        // For short-answer questions, any answer is valid.
+        return true;
+    } else if (question.type === "multiple_choice_question") {
+        // For multiple-choice questions, the answer must be one of the options.
+        return question.options.includes(answer);
+    } else {
+        // Handle other question types if needed.
+        return false;
+    }
 }
 
 /**
@@ -41,7 +60,11 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    // Get the first 10 characters of the name (or the whole name if it's shorter).
+    const shortName = question.name.substring(0, 10);
+
+    // Combine the id and short name with ": ".
+    return `${question.id}: ${shortName}`;
 }
 
 /**
@@ -62,7 +85,13 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let markdown = `# ${question.name}\n${question.body}`;
+    if (question.type === "multiple_choice_question") {
+        for (const option of question.options) {
+            markdown += `\n- ${option}`;
+        }
+    }
+    return markdown;
 }
 
 /**
@@ -70,7 +99,14 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    // Create a new question object with the same properties as the original question,
+    // but with the updated name.
+    const updatedQuestion: Question = {
+        ...question, // Copy all properties from the original question
+        name: newName // Update the name property with the new name
+    };
+
+    return updatedQuestion;
 }
 
 /**
@@ -79,7 +115,14 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    // Create a new question object with the same properties as the original question,
+    // but with the inverted 'published' field.
+    const updatedQuestion: Question = {
+        ...question, // Copy all properties from the original question
+        published: !question.published // Invert the 'published' field
+    };
+
+    return updatedQuestion;
 }
 
 /**
@@ -89,7 +132,19 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    // Create a new question object by copying over properties from the old question.
+    const duplicatedQuestion: Question = {
+        id: id, // Set the new ID
+        name: `Copy of ${oldQuestion.name}`, // Modify the name
+        body: oldQuestion.body,
+        type: oldQuestion.type,
+        options: oldQuestion.options,
+        expected: oldQuestion.expected,
+        points: oldQuestion.points,
+        published: false // Reset the published field to false
+    };
+
+    return duplicatedQuestion;
 }
 
 /**
@@ -100,9 +155,17 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
-}
+    // Create a new array containing the existing options and the new option.
+    const updatedOptions = [...question.options, newOption];
 
+    // Create a new question object with the updated options.
+    const updatedQuestion: Question = {
+        ...question, // Copy all properties from the original question
+        options: updatedOptions // Update the options property with the new array
+    };
+
+    return updatedQuestion;
+}
 /**
  * Consumes an id, name, and two questions, and produces a new question.
  * The new question will use the `body`, `type`, `options`, and `expected` of the
@@ -117,5 +180,17 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number }
 ): Question {
-    return contentQuestion;
+    // Create a new question object by merging information from contentQuestion and points.
+    const mergedQuestion: Question = {
+        id: id, // Set the new ID
+        name: name, // Set the new name
+        body: contentQuestion.body,
+        type: contentQuestion.type,
+        options: [...contentQuestion.options], // Create a new copy of options
+        expected: contentQuestion.expected,
+        points: points,
+        published: false // Set published to false
+    };
+
+    return mergedQuestion;
 }
